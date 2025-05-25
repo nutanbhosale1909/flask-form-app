b16cae13770dc8351e1385b4d0d0ae1faed2780f
nutanbhosale1909 <nutanbhosale1909@gmail.com>
2025-05-25 16:21:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,21 @@
-provider "aws" {
-  region = var.region
-}
-
-resource "aws_instance" "web" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  user_data = file("setup.sh")  # bootstrap script
-
-  tags = {
-    Name = "flask-form-server"
-  }
-
-  vpc_security_group_ids = [aws_security_group.allow_web.id]
-}
-
 resource "aws_security_group" "allow_web" {
   name        = "allow_web"
   description = "Allow HTTP and SSH"
+
   ingress = [
     {
-      description      = "HTTP"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      description = "HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     },
     {
-      description      = "SSH"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      description = "SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
     }
   ]
 
@@ -44,4 +27,16 @@ resource "aws_security_group" "allow_web" {
       cidr_blocks = ["0.0.0.0/0"]
     }
   ]
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
+  user_data              = file("setup.sh")
+
+  tags = {
+    Name = "flask-form-server"
+  }
 }
