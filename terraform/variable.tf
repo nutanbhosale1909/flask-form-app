80d2ec1d4167ac9ad2d9eee27bcf3cab3eb62fef
nutanbhosale1909 <nutanbhosale1909@gmail.com>
2025-05-25 16:23:03 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -6,10 +6,10 @@ variable "instance_type" {
   default = "t2.medium"
 }
 
-variable "key_name" {
-  description = "aws_ec2.pem"
+variable "aws1.pem" {
+  description = "Your AWS EC2 Key Pair Name"
 }
 
 variable "ami" {
-  default = "ami-0c02fb55956c7d316" # Ubuntu 20.04 LTS for us-east-1
+  default = "ami-0c02fb55956c7d316" # Ubuntu 20.04 in us-east-1
 }
