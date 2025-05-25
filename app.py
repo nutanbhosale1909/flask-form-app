e95c3e2e4d7703e5a0d5212b2b0ec1d61bb0499e
nutanbhosale1909 <nutanbhosale1909@gmail.com>
2025-05-25 09:44:45 +0000

    added_flask_app

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        first = request.form.get('first_name')
-        last = request.form.get('last_name')
-        return f"<h2>Hello, {first} {last}!</h2>"
-    return render_template('form.html')
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        return f"Hello, {first_name} {last_name}!"
+    return render_template("form.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
