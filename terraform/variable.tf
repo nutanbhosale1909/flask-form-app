99626ccd70a545664b243709e310c5b69d6c1fea
nutanbhosale1909 <nutanbhosale1909@gmail.com>
2025-06-02 13:58:58 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,15 +1,22 @@
 variable "region" {
-  default = "us-east-1"
-}
-
-variable "instance_type" {
-  default = "t2.medium"
-}
-
-variable "aws1.pem" {
-  description = "Your AWS EC2 Key Pair Name"
+  default = "ap-south-1"
 }
 
 variable "ami" {
-  default = "ami-0c02fb55956c7d316" # Ubuntu 20.04 in us-east-1
+  description = "Ubuntu AMI ID"
+  default     = "ami-00bb6a80f01f03502"
+}
+
+variable "instance_type" {
+  default = "t3.medium"
+}
+
+variable "key_name" {
+  description = "Your existing key pair name"
+  default     = "terraform_aws"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to launch the instance"
+  default     = "subnet-0a27fed6a28aeec48"
 }
